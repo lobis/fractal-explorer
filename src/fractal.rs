3b0b79c096b83d9e52409a1862f1159538477ec9
lobis <luis.antonio.obis@gmail.com>
2022-08-08 02:32:11 +0200

    added fractal image to tests

--- src/fractal.rs
+++ src/fractal.rs
@@ -8,7 +8,7 @@ pub fn julia_point(
     iteration_limit: Option<u32>,
     z_mag2_limit: Option<f64>,
 ) -> f64 {
-    let z_mag2_limit: f64 = z_mag2_limit.unwrap_or(100.0);
+    let z_mag2_limit: f64 = z_mag2_limit.unwrap_or(10.0);
     let iteration_limit: u32 = iteration_limit.unwrap_or(255);
 
     let mut z = z.clone();
@@ -27,6 +27,7 @@ pub fn julia_set(
     range: ((f64, f64), (f64, f64)),
     center: (f64, f64),
     julia_point_function: fn(Complex) -> f64,
+    color_function: Option<fn(f64) -> [u8; 3]>,
 ) -> RgbImage {
     let pixel_size = (
         (range.0 .1 - range.0 .0) / image_size_in_pixel.0 as f64,
@@ -34,7 +35,10 @@ pub fn julia_set(
     );
 
     let mut image: RgbImage = ImageBuffer::new(image_size_in_pixel.0, image_size_in_pixel.1);
-
+    let color_function = color_function.unwrap_or(|intensity| {
+        let pixel_value = (intensity * 255 as f64) as u8;
+        return [pixel_value, pixel_value, pixel_value];
+    });
     for x in 0..image_size_in_pixel.0 {
         for y in 0..image_size_in_pixel.1 {
             let z = Complex {
@@ -42,15 +46,38 @@ pub fn julia_set(
                 imaginary: range.1 .0 + y as f64 * pixel_size.1 - center.1,
             };
             let intensity = julia_point_function(z);
-            let color_intensity = (intensity * 255 as f64) as u8;
 
             let pixel = image.get_pixel_mut(x, y);
             let image::Rgb(_) = *pixel;
-            *pixel = image::Rgb([color_intensity, color_intensity, color_intensity]);
+            *pixel = image::Rgb(color_function(intensity));
         }
     }
 
     return image;
+}
+
+// TODO: not working with user submitted colors, arguments unused
+pub fn color_interpolation_generator(
+    color_start: [u8; 3], // closest to border of fractal
+    color_end: [u8; 3],   // outside of fractal
+) -> fn(f64) -> [u8; 3] {
+    return |intensity: f64| -> [u8; 3] {
+        // intensity in range [0.0, 1.0]
+        if intensity >= 1.0 {
+            return [0, 0, 0];
+        }
+
+        // linear interpolation between two colors
+        let color_start: [u8; 3] = [255, 0, 0]; // closest to border of fractal
+        let color_end: [u8; 3] = [255, 255, 0]; // outside of fractal
+
+        let rgb = [
+            (color_end[0] as f64 * intensity + color_start[0] as f64 * (1.0 - intensity)) as u8,
+            (color_end[1] as f64 * intensity + color_start[1] as f64 * (1.0 - intensity)) as u8,
+            (color_end[2] as f64 * intensity + color_start[2] as f64 * (1.0 - intensity)) as u8,
+        ];
+        return rgb;
+    };
 }
 
 #[cfg(test)]
@@ -69,5 +96,34 @@ mod tests {
         };
 
         assert_eq!(julia_point(&z_initial, &offset, None, None), 1.0);
+    }
+
+    #[test]
+    fn test_julia_set_image() {
+        let size_pixel = 512;
+        let range_half = 1.55;
+
+        fn julia_function(z: Complex) -> f64 {
+            julia_point(
+                &z,
+                &Complex {
+                    real: -0.75,
+                    imaginary: 0.0,
+                },
+                Some(25),
+                None,
+            )
+        }
+
+        let color_function = color_interpolation_generator([255, 0, 0], [255, 255, 0]);
+
+        let image = julia_set(
+            (size_pixel, size_pixel),
+            ((-range_half, range_half), (-range_half, range_half)),
+            (0.0, 0.0),
+            julia_function,
+            Some(color_function),
+        );
+        image.save("fractal.png").unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,3 @@
-use fractal_generator::{fractal::julia_point, fractal::julia_set, Complex};
-
 fn main() {
-    let size_pixel = 2048;
-    let range_half = 1.55;
-
-    fn julia_function(z: Complex) -> f64 {
-        julia_point(
-            &z,
-            &Complex {
-                real: -0.75,
-                imaginary: 0.0,
-            },
-            None,
-            None,
-        )
-    }
-    let image = julia_set(
-        (size_pixel, size_pixel),
-        ((-range_half, range_half), (-range_half, range_half)),
-        (0.0, 0.0),
-        julia_function,
-    );
-    image.save("fractal.png").unwrap();
+    println!("Not doing anything yet!")
 }
