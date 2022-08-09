7198378713d57bff8de91a2e1603e21359098df2
lobis <luis.antonio.obis@gmail.com>
2022-08-09 19:02:29 +0200

    paint on full screen

--- src/vertex.rs
+++ src/vertex.rs
@@ -28,25 +28,25 @@ impl Vertex {
 
 pub const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
+        position: [-1.0, -1.0, 0.0],
         color: [0.5, 0.0, 0.5],
-    }, // A
+    },
     Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
+        position: [1.0, 1.0, 0.0],
         color: [0.5, 0.0, 0.5],
-    }, // B
+    },
     Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
+        position: [-1.0, 1.0, 0.0],
         color: [0.5, 0.0, 0.5],
-    }, // C
+    },
     Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
+        position: [1.0, -1.0, 0.0],
         color: [0.5, 0.0, 0.5],
-    }, // D
-    Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // E
+    },
 ];
 
-pub const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /* padding */ 0];
+pub const INDICES: &[u16] = &[
+    0, 1, 2, //
+    // 1, 0, 3, //
+    /* padding */ 0,
+];
