4d3abf8fad236331572ee6eb6e9eb7b9abca2917
Luis Obis <luis.antonio.obis@gmail.com>
2022-09-16 11:48:27 +0200

    increased size of web canvas

--- src/window.rs
+++ src/window.rs
@@ -52,7 +52,7 @@ pub async fn run() {
         // Winit prevents sizing with CSS, so we have to set
         // the size manually when on web.
         use winit::dpi::PhysicalSize;
-        window.set_inner_size(PhysicalSize::new(450, 400));
+        window.set_inner_size(PhysicalSize::new(800, 800));
 
         use winit::platform::web::WindowExtWebSys;
         web_sys::window()
