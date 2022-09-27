f71c94ed93b409b3421faad751608c08b2c5ba95
Luis Obis <luis.antonio.obis@gmail.com>
2022-09-27 21:40:07 +0200

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![windows_subsystem = "windows"] // Remove this to use console for debugging
 
-use fractal_explorer::window::run;
+use fractal_explorer::window;
 
 fn main() {
-    pollster::block_on(run());
+    pollster::block_on(window::run());
 }
